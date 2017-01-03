a7357fec4461339ca3ffe3c97fb0df9012dfca35
Maël Guiraud <maelguiraud@hotmail.fr>
2017-01-03 17:16:15 +0100

    oejo

--- master/thread.h
+++
@@ -1,7 +0,0 @@
-
-void *server_listener(void *arg);
-void *server_listener_optim(void *arg);
-int* create_sockets(int * socket_desc);
-int create_threads(int * servers_id,argument * args);
-void wait_all_threads_close();
-void wait_threads(int nb_inter);
